32fcf7140d44bd1f560d7fb006e7646d8904c2ed
Oskar Lee <oskar@nordlee.com>
2025-07-17 12:22:34 -0400

    added to loglistitem created new logtemplate file which will display all log information when a specific log is created. need to still add way to navigate to logtemplate.kt after selecting a logitem in HiveTemplate.kt

---
+++ app/src/main/java/com/penguinairlines/hivetraker/ui/hives/logs/LogListItem.kt
@@ -0,0 +1,47 @@
+package com.penguinairlines.hivetraker.ui.hives.logs
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.penguinairlines.hivetraker.data.models.Log
+
+
+@Composable
+fun LogListItem(log: Log, modifier: Modifier = Modifier,onClick: () -> Unit) {
+    val dateFormat = java.text.SimpleDateFormat("yyyy-MM-dd", java.util.Locale.getDefault())
+
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(vertical = 4.dp),
+        shape = MaterialTheme.shapes.medium,
+        colors = CardDefaults.cardColors()
+    ) {
+        Column(modifier = Modifier.padding(12.dp)) {
+            // Top Section: Title + Date
+            Text(
+                text = log.logName,
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.padding(bottom = 4.dp)
+            )
+
+            Text(
+                text = "Date: ${dateFormat.format(log.date)}",
+                style = MaterialTheme.typography.labelSmall,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+
+            // Notes
+            Text(
+                text = "Notes: ${log.notes ?: "N/A"}",
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+        }
+    }
+}
