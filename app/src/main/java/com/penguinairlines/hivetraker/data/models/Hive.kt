563d33fab20f1c21832c8248a2c671f95de27930
Oskar Lee <oskar@nordlee.com>
2025-07-17 10:52:02 -0400

    New Log class

--- app/src/main/java/com/penguinairlines/hivetraker/data/models/Hive.kt
+++ app/src/main/java/com/penguinairlines/hivetraker/data/models/Hive.kt
@@ -1,14 +1,16 @@
 package com.penguinairlines.hivetraker.data.models
 
 import android.location.Location
-
+import kotlin.collections.emptyList
 data class Hive(
     val name: String,
     val yard: Yard,
     var location: Location,
     var status: HiveStatus,
     var frameCount: UInt,
-    var description: String
+    var description: String,
+    var logList: List<Log> = emptyList()
+
 ) {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
