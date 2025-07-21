145c9d3c24bec9c71721cab99ed53accc2ca9bb8
Oskar Lee <oskar@nordlee.com>
2025-07-21 11:52:58 -0400

    new add log screen file

---
+++ app/src/main/java/com/penguinairlines/hivetraker/ui/hives/logs/AddLogScreen.kt
@@ -0,0 +1,15 @@
+package com.penguinairlines.hivetraker.ui.hives.logs
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.penguinairlines.hivetraker.data.models.Hive
+import com.penguinairlines.hivetraker.data.models.Log
+
+@Composable
+fun AddLogScreen(
+    onBackClick: () -> Unit,
+    onSaveClick: (logData: Log) -> Unit,
+    modifier: Modifier = Modifier
+){
+
+}
